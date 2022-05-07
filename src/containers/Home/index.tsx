f4cfc10c3ee073d389ece37c49e903210c24c6c0
Ricardo Alcantara <alcantarafox@yahoo.com.br>
2022-05-06 22:53:37 -0300

    New message

--- src/containers/Home/index.tsx
+++ src/containers/Home/index.tsx
@@ -6,10 +6,11 @@ class Home extends React.Component {
       <main className="flex-grow">
         <div className="text-center">
           <div className="px-4 py-6 text-3xl font-bold text-gray-900">
-            Oops!
+            OpenPasswd - Open Source Password Manager!
           </div>
           <div className="px-4 py-6 text-3xl font-bold text-gray-900">
-            Under construction!
+            It's under construction, if you want to store your passwords safely
+            this might be what you are looking for
           </div>
         </div>
       </main>
